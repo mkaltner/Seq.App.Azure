ec988fbb63634cf49e00200c183a81e934353ba8
Hakan Lindestaf <hakan@lindestaf.com>
2017-03-20 10:57:28 -0700

    Added Trigger properties and updated dependencies

--- Seq.App.Azure.EventHub/EventHubReactor.cs
+++ Seq.App.Azure.EventHub/EventHubReactor.cs
@@ -17,9 +17,11 @@ namespace Seq.App.Azure.EventHub
         private string _eventProperties;
         private string _staticProperties;
         private string _tagProperties;
+        private string _triggerProperties;
         private Subject<EventData> _messagesToBeSent;
         private HashSet<string> _splitEventProperties;
         private HashSet<string> _splitTagProperties;
+        private HashSet<string> _splitTriggerProperties;
         private Dictionary<string, string> _splitStaticProperties;
 
         [SeqAppSetting(
@@ -30,7 +32,8 @@ namespace Seq.App.Azure.EventHub
         [SeqAppSetting(
             DisplayName = "Event properties",
             HelpText = "Event property name(s) (comma seperated for multiple).  If omitted, selecting a signal is highly recommended otherwise every single message logged to Seq will be sent to the Event hub.",
-            IsOptional = true)]
+            IsOptional = true,
+            InputType = SettingInputType.LongText)]
         public string EventProperties
         {
             get { return _eventProperties; }
@@ -38,28 +41,26 @@ namespace Seq.App.Azure.EventHub
             {
                 _eventProperties = value;
 
-                if (string.IsNullOrEmpty(_eventProperties))
-                    _splitEventProperties = null;
-                else
-                {
-                    _splitEventProperties = new HashSet<string>();
-                    string[] parts = _eventProperties.Split(',');
-                    foreach (string part in parts)
-                        _splitEventProperties.Add(part.Trim());
-                }
+                _splitEventProperties = new HashSet<string>();
+                string[] parts = _eventProperties.Split(',');
+                foreach (string part in parts)
+                    _splitEventProperties.Add(part.Trim());
             }
         }
 
         [SeqAppSetting(
             DisplayName = "Static properties",
             HelpText = "Static properties to include in each Event Hub message.  Format: name1=value,name2=value",
-            IsOptional = true)]
+            IsOptional = true,
+            InputType = SettingInputType.LongText)]
         public string StaticProperties
         {
             get { return _staticProperties; }
             set
             {
                 _staticProperties = value;
+
+                _splitStaticProperties = new Dictionary<string, string>();
 
                 string[] parts = (_staticProperties ?? string.Empty).Split(',');
                 foreach (string part in parts)
@@ -79,7 +80,8 @@ namespace Seq.App.Azure.EventHub
         [SeqAppSetting(
             DisplayName = "Tag properties",
             HelpText = "Tag property name(s) (comma seperated for multiple). These are added to messages that are sent, but if an event only has the tag properties (and none of the event ones) then nothing will be sent.",
-            IsOptional = true)]
+            IsOptional = true,
+            InputType = SettingInputType.LongText)]
         public string TagProperties
         {
             get { return _tagProperties; }
@@ -87,9 +89,31 @@ namespace Seq.App.Azure.EventHub
             {
                 _tagProperties = value;
 
+                _splitTagProperties = new HashSet<string>();
+
                 string[] parts = (_tagProperties ?? string.Empty).Split(',');
                 foreach (string part in parts)
                     _splitTagProperties.Add(part.Trim());
+            }
+        }
+
+        [SeqAppSetting(
+            DisplayName = "Trigger properties",
+            HelpText = "Trigger property name(s) (comma seperated for multiple). If one of these properties exists on an event then all properties on the event are sent.",
+            IsOptional = true,
+            InputType = SettingInputType.LongText)]
+        public string TriggerProperties
+        {
+            get { return _triggerProperties; }
+            set
+            {
+                _triggerProperties = value;
+
+                _splitTriggerProperties = new HashSet<string>();
+
+                string[] parts = (_triggerProperties ?? string.Empty).Split(',');
+                foreach (string part in parts)
+                    _splitTriggerProperties.Add(part.Trim());
             }
         }
 
@@ -102,9 +126,6 @@ namespace Seq.App.Azure.EventHub
 
         public AzureEventHubReactor()
         {
-            _splitStaticProperties = new Dictionary<string, string>();
-            _splitTagProperties = new HashSet<string>();
-
             _lazyClient =
                 new Lazy<EventHubClient>(() =>
                 {
@@ -148,55 +169,79 @@ namespace Seq.App.Azure.EventHub
 
         public void On(Event<LogEventData> evt)
         {
-            Dictionary<string, object> propertyData;
-
-            if (_splitEventProperties != null)
-            {
-                // If EventProperties are defined, grab matching event properties.
-                propertyData = evt.Data.Properties.Where(x => _splitEventProperties.Contains(x.Key))
-                    .ToDictionary(x => x.Key, x => GetValue(x.Value));
-            }
-            else
-            {
-                // If no EventProperties, get all properties for this event.
-                // Hopefully the user specified a signal to match because this can/will send everything!
-                propertyData = evt.Data.Properties.ToDictionary(x => x.Key, x => GetValue(x.Value));
-            }
-
-            // If no properties were found (matching or otherwise), return
-            if (!propertyData.Any())
-                return;
-
-            // Get tag properties
-            foreach (var tagProp in evt.Data.Properties.Where(x => _splitTagProperties.Contains(x.Key)))
-                propertyData[tagProp.Key] = GetValue(tagProp.Value);
-
             try
             {
-                // Parse and add static properties
-                if (_splitStaticProperties != null)
+                Dictionary<string, object> propertyData = null;
+
+                bool includeAllProperties = false;
+
+                if (_splitEventProperties != null && _splitEventProperties.Any())
                 {
-                    foreach (var kvp in _splitStaticProperties)
-                        propertyData[kvp.Key] = GetValue(kvp.Value);
+                    // If EventProperties are defined, grab matching event properties.
+                    propertyData = evt.Data.Properties.Where(x => _splitEventProperties.Contains(x.Key))
+                        .ToDictionary(x => x.Key, x => GetValue(x.Value));
                 }
+                else
+                {
+                    // If no EventProperties, get all properties for this event.
+                    // Hopefully the user specified a signal to match because this can/will send everything!
+                    includeAllProperties = true;
+                }
+
+                if (_splitTriggerProperties != null)
+                {
+                    includeAllProperties = evt.Data.Properties.Any(x => _splitTriggerProperties.Contains(x.Key));
+                }
+
+                if (includeAllProperties)
+                {
+                    propertyData = evt.Data.Properties.ToDictionary(x => x.Key, x => GetValue(x.Value));
+                }
+
+                // If no properties were found (matching or otherwise), return
+                if (propertyData == null || !propertyData.Any())
+                    return;
+
+                if (_splitTagProperties != null)
+                {
+                    // Get tag properties
+                    foreach (var tagProp in evt.Data.Properties.Where(x => _splitTagProperties.Contains(x.Key)))
+                        propertyData[tagProp.Key] = GetValue(tagProp.Value);
+
+                }
+
+                try
+                {
+                    // Parse and add static properties
+                    if (_splitStaticProperties != null)
+                    {
+                        foreach (var kvp in _splitStaticProperties)
+                            propertyData[kvp.Key] = GetValue(kvp.Value);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, "An error occurred while processing static properties.");
+                    throw;
+                }
+
+                // Add a Timestamp to all messages
+                propertyData.Add("Timestamp", evt.TimestampUtc);
+
+                var message = JsonConvert.SerializeObject(propertyData);
+
+                if (LogMessages)
+                    Log.Information("Queuing {Message}", message);
+
+                // Construct and queue the event data
+                var eventData = new EventData(Encoding.UTF8.GetBytes(message));
+                _messagesToBeSent.OnNext(eventData);
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "An error occurred while processing static properties.");
+                Log.Error(ex, "An error occurred while processing event data. " + ex.Message);
                 throw;
             }
-
-            // Add a Timestamp to all messages
-            propertyData.Add("Timestamp", evt.TimestampUtc);
-
-            var message = JsonConvert.SerializeObject(propertyData);
-
-            if (LogMessages)
-                Log.Information("Queuing {Message}", message);
-
-            // Construct and send the event data
-            var eventData = new EventData(Encoding.UTF8.GetBytes(message));
-            _messagesToBeSent.OnNext(eventData);
         }
     }
 }
